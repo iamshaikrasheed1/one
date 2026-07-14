2ef273c5ea1b0d2a3ace2b052643982edd8ca3ca
iamshaikrasheed1 <iamshaikrasheed1@gmail.com>
2026-07-14 09:57:10 +0530

    Add console log for portfolio load event

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,1 +1,1 @@
-const t="Rasheed Shaik | Oracle DBA & DevOps Engineer";let i=0;function type(){if(i<=t.length){document.getElementById("typing").textContent=t.slice(0,i++);setTimeout(type,60);}}window.onload=type;
+console.log('Portfolio Loaded');
